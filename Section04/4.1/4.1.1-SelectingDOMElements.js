9550c4aa356b23eea58b5250827010b4b8e8a0b5
Doc Carson <kvcarsonjr@gmail.com>
2023-02-01 09:36:21 -0500

    section04 class activities

--- Section04/4.1/4.1.1-SelectingDOMElements.js
+++ Section04/4.1/4.1.1-SelectingDOMElements.js
@@ -1,7 +1,8 @@
 // select all unordered list (ul) elements
-
+document.getElementsByTagName("ul");
 // select all elements with the class "class-week"
-
+document.getElementsByClassName("class-week");
 // select all elements with the class "nav-bar"
-
+document.getElementsByClassName("nav-bar");
 // select the element with the id "dog-picture" and save it to a variable
+document.getElementById("dog-picture");
