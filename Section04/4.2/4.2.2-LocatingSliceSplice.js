14a143fff85241a59b63ccba4ab9de25f9c91848
Doc Carson <kvcarsonjr@gmail.com>
2023-02-11 22:59:09 -0500

    updates

--- Section04/4.2/4.2.2-LocatingSliceSplice.js
+++ Section04/4.2/4.2.2-LocatingSliceSplice.js
@@ -1,8 +1,25 @@
 const arr = ["a", "a", "b", "a", "c", "b", "a", "b", "b", "c", "c", "c"];
 
 // find the first index of "a", "b", and "c"
+let firstA = arr.indexOf("a");
+let firstB = arr.indexOf("b");
+let firstC = arr.indexOf("c");
+
+console.log(firstA + " " + firstB + " " + firstC);
 
 // find the last index of "a", "b", and "c"
+let firstA = arr.lastIndexOf("a");
+let firstB = arr.lastIndexOf("b");
+let firstC = arr.lastIndexOf("c");
+
+console.log(lastA + " " + lastB + " " + lastC);
 
 // if the first index and last index of "a" is not the same, remove the last instance
 // repeat until there is just one "a"
+function removeDuplicates(array, duplicatedValues){
+ let firstInstance = array.indexOf(duplicatedValues)
+  if (array.indexOf(duplicatedValues) !== array.lastIndexOf(duplicatedValues)) {
+
+ }
+}
+removeDuplicates(arr, "a");
