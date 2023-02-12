f83cd329a368461d9e970dbf2b4098828c0ed7d7
Doc Carson <kvcarsonjr@gmail.com>
2023-02-12 00:26:31 -0500

    updates

--- Section04/4.2/4.2.1-PopPushShiftUnshift.js
+++ Section04/4.2/4.2.1-PopPushShiftUnshift.js
@@ -1,7 +1,10 @@
 const nums = [6, 5, 2, 3, 4, 1, 0];
 // remove each of the last two items with pop(), saving each item to a variable
-let removeNumberOne = nums.pop();
+const removeLastItem1 = nums.pop();
+const removeLastItem2 = nums.pop();
 // remove each of the first two items with shift(), saving each item to a variable
 const removeFrontItem1 = nums.shift();
 const removeFrontItem2 = nums.shift();
 // use push and unshift to add the variables back to the array in numerical order, 0-6
+nums.unshift(removeLastItem1, removeLastItem2);
+nums.push(removeFrontItem2, removeFrontItem1);
--- Section04/4.2/4.2.2-LocatingSliceSplice.js
+++ Section04/4.2/4.2.2-LocatingSliceSplice.js
@@ -5,21 +5,35 @@ let firstA = arr.indexOf("a");
 let firstB = arr.indexOf("b");
 let firstC = arr.indexOf("c");
 
-console.log(firstA + " " + firstB + " " + firstC);
-
+console.log(
+  `The first index of 'a' is ${firstA}, the first index of 'b' is ${firstB}, and the first index of 'c' is ${firstC}`
+);
 // find the last index of "a", "b", and "c"
-let firstA = arr.lastIndexOf("a");
-let firstB = arr.lastIndexOf("b");
-let firstC = arr.lastIndexOf("c");
+let lastA = arr.lastIndexOf("a");
+let lastB = arr.lastIndexOf("b");
+let lastC = arr.lastIndexOf("c");
 
-console.log(lastA + " " + lastB + " " + lastC);
-
+console.log(
+  `The last index of 'a' is ${lastA}, the last index of 'b' is ${lastB}, and the last index of 'c' is ${lastC}`
+);
 // if the first index and last index of "a" is not the same, remove the last instance
 // repeat until there is just one "a"
-function removeDuplicates(array, duplicatedValues){
- let firstInstance = array.indexOf(duplicatedValues)
-  if (array.indexOf(duplicatedValues) !== array.lastIndexOf(duplicatedValues)) {
 
- }
+function removeDuplicates(array, duplicatedValues) {
+  let firstInstance = array.indexOf(duplicatedValues);
+  let lastInstance = array.lastIndexOf(duplicatedValues);
+
+  while (firstInstance !== lastInstance) {
+    array.splice(lastInstance, 1);
+    lastInstance = array.lastIndexOf(duplicatedValues);
+  }
+
+  return array;
 }
-removeDuplicates(arr, "a");
+
+let singleAList = removeDuplicates(arr, "a");
+console.log(singleAList);
+let singleB = removeDuplicates(arr, "b");
+console.log(singleB);
+let singleC = removeDuplicates(arr, "c");
+console.log(singleC);
